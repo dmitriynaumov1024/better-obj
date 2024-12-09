755b68a5c758c2464fff96e74e4535c09633d47c
Dmitriy Naumov <naumov1024@gmail.com>
2024-12-09 14:06:00 +0200

    /* data types

--- src/datatype.js
+++ src/datatype.js
@@ -2,13 +2,13 @@
 // something in between sql and js data types
 //
 const type = {
-    Integer: "INTEGER",
+    Integer: "BIGINT",
     String: "STRING",
     Date: "TIMESTAMP",
     DateTime: "TIMESTAMP",
     Timestamp: "TIMESTAMP",
     Float: "FLOAT",
-    Double: "DOUBLE",
+    Double: "DOUBLE PRECISION",
     Boolean: "BOOLEAN",
     Object: "OBJECT"
 }
